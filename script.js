ea4201d21b4b3683f1b390019f436a57f89259de
MDWADUD <abdullahallwadud@live.com>
2026-09-07 14:21:29 -0400

    Update portfolio homepage

---
+++ script.js
@@ -0,0 +1,30 @@
+const roles = [
+    "System Analyst",
+    "Developer",
+    "IAM Engineer",
+    "Software Tester"
+];
+
+let i = 0;
+
+setInterval(() => {
+    i = (i + 1) % roles.length;
+    document.getElementById("roleText").innerHTML = roles[i];
+}, 400);
+
+setTimeout(() => {
+    const loadingDots = document.getElementById("loadingDots");
+
+    let dotCount = 1;
+
+    setInterval(() => {
+        dotCount++;
+
+        if (dotCount > 3) {
+            dotCount = 1;
+        }
+
+        loadingDots.textContent = ".".repeat(dotCount);
+    }, 500);
+
+}, 5000);
